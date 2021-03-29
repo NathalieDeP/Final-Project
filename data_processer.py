bc549d3db7af4d26bc47913f8aa0dc689ef7e99f
NathalieDeP <depalmnathalie@gmail.com>
2021-03-29 09:47:31 -0400

    removed unneeded print statement

--- data_processer.py
+++ data_processer.py
@@ -23,14 +23,13 @@ import sys
 	
 def main(argv):
     if len(argv) != 3:
-        print('Please enter the idiom-, 2015 data-, and 2020 data files in the command line while running this program.', file=sys.stderr)
+        print('Please enter the idiom-, 2015 data-, or 2020 data files in the command line while running this program.', file=sys.stderr)
         exit(-1)
 		
     idioms_file = open(argv[1], 'r')
     idioms = idioms_file.readlines()
     idioms_file.close()
     with open(argv[2], 'r') as data:
-#    idiom_count, non_idiom_count = process(data, idioms)
         idiom_counter = 0
         non_idiom_counter = 0
         for tweet in data:
@@ -42,7 +41,6 @@ def main(argv):
                     chkpnt = 1
             if chkpnt == 0:
                 non_idiom_counter += 1
-            print(idiom_counter + non_idiom_counter, idiom_counter)
     print('Tweets with Dutch idioms:', idiom_counter)
     print('Tweets without Dutch idioms:', non_idiom_counter)
     print('Total tweets:', (idiom_counter + non_idiom_counter))
